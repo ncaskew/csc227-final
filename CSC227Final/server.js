fb1c154020000da2fc198097bd5ed8373ebab202
ncaskew <78122473+ncaskew@users.noreply.github.com>
2022-04-29 20:54:25 -0400

    Update port and ipaddress to use OpenShift environment variables

--- CSC227Final/server.js
+++ CSC227Final/server.js
@@ -1,8 +1,10 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 1337;
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+}).listen(port, ipaddress);
